5efe9aded536daae55e4c05de71440026150779a
Jun <pasingjun@gmail.com>
2026-05-13 10:31:00 +0800

    Version 3 - Added functions

--- grade_calculator.c
+++ grade_calculator.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+float computeAverage(float prelim, float midterm, float finals) {
+    return (prelim + midterm + finals) / 3;
+}
+
 char getLetterGrade(float average) {
     if (average >= 90)
         return 'A';
@@ -13,13 +17,26 @@ char getLetterGrade(float average) {
         return 'F';
 }
 
+void displayResult(char name[], int id,
+                   float average, char letterGrade) {
+
+    printf("\n=== GRADE REPORT ===\n");
+    printf("Name: %s", name);
+    printf("Student ID: %d\n", id);
+    printf("Average: %.2f\n", average);
+    printf("Letter Grade: %c\n", letterGrade);
+
+    if (average >= 75)
+        printf("Remarks: PASSED\n");
+    else
+        printf("Remarks: FAILED\n");
+}
+
 int main() {
     char studentName[50];
     int studentID;
     float prelim, midterm, finals, average;
     char letterGrade;
-
-    printf("=== STUDENT GRADE CALCULATOR ===\n");
 
     printf("Enter student name: ");
     fgets(studentName, sizeof(studentName), stdin);
@@ -36,19 +53,11 @@ int main() {
     printf("Enter Finals score: ");
     scanf("%f", &finals);
 
-    average = (prelim + midterm + finals) / 3;
+    average = computeAverage(prelim, midterm, finals);
     letterGrade = getLetterGrade(average);
 
-    printf("\n=== GRADE REPORT ===\n");
-    printf("Name: %s", studentName);
-    printf("Student ID: %d\n", studentID);
-    printf("Average: %.2f\n", average);
-    printf("Letter Grade: %c\n", letterGrade);
-
-    if (average >= 75)
-        printf("Remarks: PASSED\n");
-    else
-        printf("Remarks: FAILED\n");
+    displayResult(studentName, studentID,
+                  average, letterGrade);
 
     return 0;
 }
